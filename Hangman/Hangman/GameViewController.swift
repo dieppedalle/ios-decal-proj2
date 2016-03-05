d2fb6229e911d5c0e17a85d65d7bd291a4dd537d
Gauthier Dieppedalle <Gauthier@Gauthiers-MacBook-Pro.local>
2016-03-05 00:10:47 -0800

    First Commit

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -9,13 +9,100 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    @IBOutlet weak var wordReveal: UILabel!
+    @IBOutlet weak var A: UIButton!
+    
+    @IBOutlet weak var B: UIButton!
+    
+    @IBOutlet weak var C: UIButton!
+    
+    @IBOutlet weak var D: UIButton!
 
+    @IBOutlet weak var E: UIButton!
+    
+    @IBOutlet weak var F: UIButton!
+    
+    @IBOutlet weak var G: UIButton!
+    
+    @IBOutlet weak var H: UIButton!
+    
+    @IBOutlet weak var I: UIButton!
+    
+    @IBOutlet weak var J: UIButton!
+    
+    @IBOutlet weak var K: UIButton!
+    
+    @IBOutlet weak var L: UIButton!
+    
+    @IBOutlet weak var M: UIButton!
+    
+    @IBOutlet weak var N: UIButton!
+    
+    @IBOutlet weak var O: UIButton!
+    
+    @IBOutlet weak var P: UIButton!
+    
+    @IBOutlet weak var Q: UIButton!
+    
+    @IBOutlet weak var R: UIButton!
+    
+    @IBOutlet weak var S: UIButton!
+    
+    @IBOutlet weak var T: UIButton!
+    
+    @IBOutlet weak var U: UIButton!
+    
+    @IBOutlet weak var V: UIButton!
+    
+    @IBOutlet weak var W: UIButton!
+    
+    @IBOutlet weak var X: UIButton!
+    
+    @IBOutlet weak var Y: UIButton!
+    
+    @IBOutlet weak var Z: UIButton!
+    
+    //var charactersPhrase = [Character]()
+    var phrase: String = ""
+    var characterAdded = [Character]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()
+        
+        var wordLabel = ""
+        
+        let stringLength = phrase.characters.count
+        var currentPosition = 0
+        
+        for character in phrase.characters{
+            currentPosition += 1
+            if character == " "{
+                wordLabel += " "
+            }
+            else{
+                if currentPosition != stringLength{
+                    wordLabel += "_ "
+                }
+                else{
+                    wordLabel += "_"
+                }
+            }
+            
+        }
+        
+        wordReveal.text = wordLabel
+        /*else if Array(arrayLiteral: wordReveal.text)[currentPosition-1] != "_"{
+            
+        }*/
+        
+        print(wordReveal.text![wordReveal.text!.startIndex.advancedBy(0)])
+        //print(Array(arrayLiteral: wordReveal.text!)[0])
         print(phrase)
     }
 
@@ -23,6 +110,62 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buttonPressed(button : UIButton)
+    {
+        print(button.currentTitle!)
+        
+        var currentPosition = 0
+        let stringLength = phrase.characters.count
+        var wordLabel = ""
+        
+        
+        for character in phrase.characters{
+            var charWordLabel = wordReveal.text![wordReveal.text!.startIndex.advancedBy(0)]
+            currentPosition += 1
+            
+            if character == " "{
+                wordLabel += " "
+            }
+            else{
+                if String(character)==button.currentTitle{
+                    wordLabel += String(character)
+                    characterAdded.append(character)
+                }
+                else if (characterAdded.contains(character)){
+                    wordLabel += String(character)
+                }
+                else{
+                    if currentPosition != stringLength{
+                        wordLabel += "_ "
+                    }
+                    else{
+                        wordLabel += "_"
+                    }
+                }
+            }
+        }
+        
+        wordReveal.text = wordLabel
+        button.enabled = false
+        
+        var allLettersGuessed = true
+        
+        for character in wordReveal.text!.characters{
+            if character == "_"{
+                allLettersGuessed = false
+            }
+        }
+        
+        if allLettersGuessed == true{
+            let alert = UIAlertController(title: "Congratulations", message: "You Won!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "New Game", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
     
 
     /*
